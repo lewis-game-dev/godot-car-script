2495afb678e41a3d85b47333530861f511eb1dad
Comet arts entertainment <77471199+lewis-game-dev@users.noreply.github.com>
2021-03-08 19:20:53 +0000

    Update car.gd

--- car.gd
+++ car.gd
@@ -24,13 +24,6 @@ var floor_normal
 
 var fr_wheel_start_rotation
 var fl_wheel_start_rotation
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		$Camera.rotation_degrees.y -= event.relative.x*0.3
-		
-	if Input.is_action_pressed("ui_cancel"):
-		get_tree().quit()
 	
 func _ready():
 	scale_object = scale
